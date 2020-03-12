371d96bb00c356b27ee8cc85eab640cf29acb248
Aleksandra Reshetnikova <ms.alexandra.arkhipova@gmail.com>
2020-03-12 21:42:20 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+        double TicketCost = 1534.35;
+        int Rate = 20;
+        int Miles = (int) TicketCost / Rate;
+        System.out.println(Miles);
+    }
+}
